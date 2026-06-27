32c5926fe6cbbdff0a8d4bd3ebdb312ab90c89e9
sadraonthehack <sadra.marafe@gmail.com>
2026-06-27 23:38:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ API_ID = 27029926
 API_HASH = "6963d3bf5f8a776f5139d71cfc707abc"
 PHONE_NUMBER = "+8801940146782"
 
-OWNERS = {"usernames": ["DevilWillCryBitch","MY_FALAH_M", "PV_KiTANAM","Pxcio","DevilWillCry1Bitch", "Pv_TERlYAKM"]}
+OWNERS = {"usernames": ["DevilWillCryBitch","MY_FALAH_M", "PV_KiTANAM","Pv_TERlYAKM"]}
 
 BOT_DIR = "downloads_bot1"
 if not os.path.exists(BOT_DIR):
